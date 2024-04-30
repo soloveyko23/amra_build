ac6ffe64cf5147e0ed67b661bc16ac78b07dbfe1
Maksym Soloviov <42580651+soloveyko23@users.noreply.github.com>
2024-04-30 13:33:20 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3379,42 +3379,8 @@
             }));
         }
     }), 0);
-    const audioPlayer = () => {
-        let buttonPlayPause = document.querySelector(".audio__button-play");
-        const wavesurfer = WaveSurfer.create({
-            container: "#waveform",
-            height: 36,
-            width: "100%",
-            splitChannels: false,
-            normalize: false,
-            waveColor: "#8b8b8b",
-            progressColor: "#fff",
-            cursorColor: "#ddd5e9",
-            cursorWidth: 2,
-            barWidth: 3,
-            barGap: 3,
-            barRadius: 6,
-            barHeight: 1,
-            minPxPerSec: 20,
-            fillParent: false,
-            url: "./img/audio.mp3",
-            autoplay: false,
-            interact: true,
-            dragToSeek: true,
-            hideScrollbar: true,
-            audioRate: 1,
-            autoScroll: true,
-            autoCenter: true,
-            sampleRate: 8e3
-        });
-        if (buttonPlayPause) buttonPlayPause.addEventListener("click", (() => {
-            wavesurfer.playPause();
-        }));
-    };
     document.addEventListener("DOMContentLoaded", (event => {
-        audioPlayer();
         if (window.matchMedia("(min-width: 767.98px)").matches) {
-            console.clear();
             gsap.registerPlugin(ScrollTrigger);
             const cardsWrappers = gsap.utils.toArray(".card-wrapper");
             const cards = gsap.utils.toArray(".card");
